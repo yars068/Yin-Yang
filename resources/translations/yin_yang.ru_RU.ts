c504881471f141fcfcdd3211a1913b657bf10ac8
Yaroslav Silyuev <yars068@yandex.ru>
2024-11-15 08:42:57 +0300

    Update translation: ru_RU: resources/translations/yin_yang.ru_RU.ts

--- resources/translations/yin_yang.ru_RU.ts
+++ resources/translations/yin_yang.ru_RU.ts
@@ -4,82 +4,87 @@
 <context>
     <name>main_window</name>
     <message>
-        <location filename="../../designer/main_window.ui" line="50"/>
+        <location filename="../../designer/main_window.ui" line="51"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="56"/>
+        <location filename="../../designer/main_window.ui" line="57"/>
         <source>Automatic theme switching</source>
         <translation>Автоматически переключать тему</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="76"/>
+        <location filename="../../designer/main_window.ui" line="77"/>
         <source>Custom Schedule</source>
         <translation>Задать расписание</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="176"/>
+        <location filename="../../designer/main_window.ui" line="177"/>
         <source>Sunset to Sunrise</source>
         <translation>От заката до восхода</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="101"/>
+        <location filename="../../designer/main_window.ui" line="102"/>
         <source>Light:</source>
         <translation>День:</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="137"/>
+        <location filename="../../designer/main_window.ui" line="138"/>
         <source>Dark:</source>
         <translation>Ночь:</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="189"/>
+        <location filename="../../designer/main_window.ui" line="190"/>
         <source>Longitude:</source>
         <translation>Долгота:</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="224"/>
+        <location filename="../../designer/main_window.ui" line="225"/>
         <source>Latitude:</source>
         <translation>Широта:</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="262"/>
+        <location filename="../../designer/main_window.ui" line="263"/>
         <source>update location automatically</source>
         <translation>Автоматически обновлять местоположение</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="285"/>
+        <location filename="../../designer/main_window.ui" line="286"/>
         <source>Light</source>
         <translation>Светлая</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="292"/>
+        <location filename="../../designer/main_window.ui" line="293"/>
         <source>Dark</source>
         <translation>Тёмная</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="302"/>
+        <location filename="../../designer/main_window.ui" line="303"/>
         <source>Send a notification</source>
         <translation>Показывать уведомление</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="311"/>
+        <location filename="../../designer/main_window.ui" line="315"/>
         <source>Time to wait until the system finished booting. Default value is 10 seconds.</source>
         <translation>Время ожидания окончания загрузки системы. По умолчанию 10 секунд.</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="314"/>
+        <location filename="../../designer/main_window.ui" line="318"/>
         <source>Delay after boot:</source>
         <translation>Задержка после загрузки:</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="321"/>
+        <location filename="../../designer/main_window.ui" line="325"/>
         <source>s</source>
         <translation>сек</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="354"/>
+        <location filename="../../designer/main_window.ui" line="337"/>
+        <source>Dark mode will be active between</source>
+        <translation>Тёмный режим будет активен между</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="345"/>
         <source>Plugins</source>
         <translation>Плагины</translation>
     </message>
@@ -87,19 +92,16 @@
 <context>
     <name>systray</name>
     <message>
-        <location filename="../../yin_yang/__main__.py" line="127"/>
         <source>Open Yin Yang</source>
         <comment>Context menu action in the systray</comment>
         <translation>Открыть Yin-Yang</translation>
     </message>
     <message>
-        <location filename="../../yin_yang/__main__.py" line="130"/>
         <source>Toggle theme</source>
         <comment>Context menu action in the systray</comment>
         <translation>Переключить тему</translation>
     </message>
     <message>
-        <location filename="../../yin_yang/__main__.py" line="133"/>
         <source>Quit</source>
         <comment>Context menu action in the systray</comment>
         <translation>Выход</translation>
