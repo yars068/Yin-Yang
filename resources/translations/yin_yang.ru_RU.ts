34c614fcbea0af6868eaa352c165b735ef565168
Yaroslav Silyuev <yars068@yandex.ru>
2024-11-15 06:10:14 +0300

    Add new translation: ru_RU: resources/translations/yin_yang.ru_RU.ts

---
+++ resources/translations/yin_yang.ru_RU.ts
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="en_GB">
+<context>
+    <name>main_window</name>
+    <message>
+        <location filename="../../designer/main_window.ui" line="50"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="56"/>
+        <source>Automatic theme switching</source>
+        <translation>Автоматически переключать тему</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="76"/>
+        <source>Custom Schedule</source>
+        <translation>Задать расписание</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="176"/>
+        <source>Sunset to Sunrise</source>
+        <translation>От заката до восхода</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="101"/>
+        <source>Light:</source>
+        <translation>День:</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="137"/>
+        <source>Dark:</source>
+        <translation>Ночь:</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="189"/>
+        <source>Longitude:</source>
+        <translation>Долгота:</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="224"/>
+        <source>Latitude:</source>
+        <translation>Широта:</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="262"/>
+        <source>update location automatically</source>
+        <translation>Автоматически обновлять местоположение</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="285"/>
+        <source>Light</source>
+        <translation>Светлая</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="292"/>
+        <source>Dark</source>
+        <translation>Тёмная</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="302"/>
+        <source>Send a notification</source>
+        <translation>Показывать уведомление</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="311"/>
+        <source>Time to wait until the system finished booting. Default value is 10 seconds.</source>
+        <translation>Время ожидания окончания загрузки системы. По умолчанию 10 секунд.</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="314"/>
+        <source>Delay after boot:</source>
+        <translation>Задержка после загрузки:</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="321"/>
+        <source>s</source>
+        <translation>сек</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="354"/>
+        <source>Plugins</source>
+        <translation>Плагины</translation>
+    </message>
+</context>
+<context>
+    <name>systray</name>
+    <message>
+        <location filename="../../yin_yang/__main__.py" line="127"/>
+        <source>Open Yin Yang</source>
+        <comment>Context menu action in the systray</comment>
+        <translation>Открыть Yin-Yang</translation>
+    </message>
+    <message>
+        <location filename="../../yin_yang/__main__.py" line="130"/>
+        <source>Toggle theme</source>
+        <comment>Context menu action in the systray</comment>
+        <translation>Переключить тему</translation>
+    </message>
+    <message>
+        <location filename="../../yin_yang/__main__.py" line="133"/>
+        <source>Quit</source>
+        <comment>Context menu action in the systray</comment>
+        <translation>Выход</translation>
+    </message>
+</context>
+</TS>
